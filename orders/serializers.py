dfa9bc40f39d5e169003255e91de155f2a6c79dd
robertoarr <roberto.ramirez.rjo@gmail.com>
2019-03-28 21:37:01 -0600

    Tests

--- orders/serializers.py
+++ orders/serializers.py
@@ -9,7 +9,7 @@ from .models import Order
 
 class OrderSerializer(serializers.ModelSerializer):
     created = serializers.SerializerMethodField()
-    test = serializers.BooleanField(read_only=True, required=True)
+    test = serializers.BooleanField(write_only=True, required=True)
 
     class Meta:
         model = Order
